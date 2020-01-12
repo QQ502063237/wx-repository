d0d62c831a5580194a1246108998cb2f50a09114
QQ502063237 <49181051+QQ502063237@users.noreply.github.com>
2020-01-12 09:32:21 +0800

    Update DemoApplication.java

--- demo/src/main/java/com/weixin/demo/DemoApplication.java
+++ demo/src/main/java/com/weixin/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-        System.out.println("启动项目成功");
+        System.out.println("启动项目成功......");
     }
 
 }
