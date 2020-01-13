bb114affed94ec8969b7b3bdd443f191752dbfdc
wn <502063237@qq.com>
2020-01-13 11:21:25 +0800

    注释拦截器

--- demo/src/main/java/com/weixin/demo/checkfilter/CheckRequest.java
+++ demo/src/main/java/com/weixin/demo/checkfilter/CheckRequest.java
@@ -10,6 +10,6 @@ public class CheckRequest implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         //super.addInterceptors(registry);
         //拦截所有请求, (提供接口的请求不需要过滤器校验 ,添加白名单)
-        registry.addInterceptor(new CheckFilter()).addPathPatterns("/**");
+//        registry.addInterceptor(new CheckFilter()).addPathPatterns("/**");
     }
 }
