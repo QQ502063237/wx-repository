b20dd96b4d25a76340da68c2c7477f921fc193cc
wn <502063237@qq.com>
2020-01-14 09:42:21 +0800

    注释拦截器

--- demo/src/main/java/com/weixin/demo/DemoApplication.java
+++ demo/src/main/java/com/weixin/demo/DemoApplication.java
@@ -12,7 +12,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) throws Exception {
-
         SpringApplication.run(DemoApplication.class, args);
         Logger logger = LoggerFactory.getLogger(DemoApplication.class);
         String result = Menu.setButtons();
